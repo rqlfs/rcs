34c4a66317d908e5d64e6289ed48592aee073c6f
rqlfs <ralphfdesigns@gmail.com>
2020-10-30 14:10:07 +0200

    Initial check-in of module rigacodingschool

---
+++ day1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Task1()
+        {
+            String name = "Ralfs";
+            String lastname = "Fadejevs";
+
+            Console.WriteLine(name + " " + lastname);
+        }
+
+        static void Task2()
+        {
+           
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Task1(); 
+
+        }
+    }
+}
